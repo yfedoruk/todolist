e062dbd14582782eb02c7685b578e8af53976e48
Fedoruk Yaroslav <y.fedoruk@favbet.com>
2020-01-18 13:51:54 +0200

    --

--- validate/regexp.go
+++ validate/regexp.go
@@ -1,15 +1,8 @@
 package validate
 
 import (
-	"net/http"
 	"regexp"
 )
-
-func Validate(f http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		f(w, r)
-	}
-}
 
 func Check(str string, rule string) bool {
 	var rx = regexp.MustCompile(rule)
