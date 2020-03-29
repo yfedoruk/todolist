d6aa1e5bc5d9daf5d7d51aba513bb8ad6338d62e
Fedoruk Yaroslav <y.fedoruk@favbet.com>
2020-03-29 13:23:15 +0300

    --

--- heroku.sh
+++ heroku.sh
@@ -2,4 +2,4 @@
 #heroku container:login
 heroku container:push --app nameless-brook-78889 web
 heroku container:release --app nameless-brook-78889 web
-heroku logs --tailgc --app nameless-brook-78889
+heroku logs --tail --app nameless-brook-78889
